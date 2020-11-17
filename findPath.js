0b29f4ad6c883624a7efc57f9b92295b1b6c401b
Hanna Seithe <hannaseithe@googlemail.com>
2020-11-17 20:06:29 +0100

    Changes to findPath; finding Path from 0,0 to 0,2

--- findPath.js
+++ findPath.js
@@ -23,98 +23,98 @@ const DIRECTION = {
     left: 1
 }
 
-function isHalfPath(card, direction) { // direction: 1 -> left, 2 -> bottom, 3 -> right
-    switch (card.shape) {
-        case 0:
-            return ((card.orientation + direction) % 4 == 0)
+function isHalfPath(card,direction) { // direction: 1 -> left, 2 -> bottom, 3 -> right
+        switch (card.shape) {
+            case 0:
+                return ((card.orientation + direction) % 4 == 0) 
                 || ((card.orientation + direction) % 4 == 3);
-        case 1:
-            return ((card.orientation + direction) % 4 == 0)
-                || ((card.orientation + direction) % 4 == 2);
-        case 2:
-            return (card.orientation + direction) % 4 == 0
-                || (card.orientation + direction) % 4 == 2
-                || (card.orientation + direction) % 4 == 3;
-        case 3:
-            return true;
-        default:
-            //throw exception;
-            break;
-    }
-}
-
-function isPath(cindex1, cindex2, lab) {
-    var isPath = true;
-    var direction;
-    //direction = cindex1[0] > cindex2[0] : 
-    if (cindex1[0] > cindex2[0]) {
-        return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.top) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.bottom)
-    } else if (cindex1[0] < cindex2[0]) {
-        return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.bottom) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.top)
-    } else if (cindex1[1] > cindex2[1]) {
-        return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.left) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.right)
-    } else if (cindex1[1] < cindex2[1]) {
-        return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.right) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.left)
-    } else {
-        //throw exception
-    }
-
-
-}
-
-function cardNotinPath(index, path) {
-    var inPath = true
-    path.forEach((card) => {
-        if ((card[0] == index[0]) && (card[1] == index[1])) {
-            inPath = false;
-        }
-    })
-    return inPath;
-}
-
-/***
-card11,card12,card13 / card21,card22, card23 / card31, card32, card33
-player is at card31 and wants to move to card13 
-test top > right > bottom > left
-***/
-
-export var potentialPath = [];
-export function findPath(startindex, goalindex, lab, path) {
-
-    console.log(path);
-
-    //test up
-    if (startindex[0] > 0 && isPath(startindex, [startindex[0] - 1, startindex[1]], lab)) {
-        if (((startindex[0] - 1) == goalindex[0]) && (startindex[1] == goalindex[1])) {
-            return true;
-        } else if (cardNotinPath([startindex[0] - 1, startindex[1]], path)) {
-            if (findPath([startindex[0] - 1, startindex[1]], goalindex, lab, [...path, [startindex[0] - 1, startindex[1]]])) { return true }
+            case 1:
+                return ((card.orientation + direction) % 4 == 0) 
+                    || ((card.orientation + direction) % 4 == 2);
+            case 2:
+                return (card.orientation + direction) % 4 == 0 
+                    || (card.orientation + direction) % 4 == 2 
+                    || (card.orientation + direction) % 4 == 3;
+            case 3:
+                return true;
+            default:
+                //throw exception;
+                break;
         }
     }
-    // test right
-    if (startindex[1] < lab[0].length - 1 && isPath(startindex, [startindex[0], startindex[1] + 1], lab)) {
-        if (((startindex[0]) == goalindex[0]) && (startindex[1] + 1 == goalindex[1])) {
-            return true;
-        } else if (cardNotinPath([startindex[0], startindex[1] + 1], path)) {
-            if (findPath([startindex[0], startindex[1] + 1], goalindex, lab, [...path, [startindex[0], startindex[1] + 1]])) { return true }
-        }
-    }
-    // test down
-    if (startindex[0] < lab.length - 1 && isPath(startindex, [startindex[0] + 1, startindex[1]], lab)) {
-        if (((startindex[0] + 1) == goalindex[0]) && (startindex[1] == goalindex[1])) {
-            return true;
-        } else if (cardNotinPath([startindex[0] + 1, startindex[1]], path)) {
-            if (findPath([startindex[0] + 1, startindex[1]], goalindex, lab, [...path, [startindex[0] + 1, startindex[1]]])) { return true }
-        }
-    }
-    // test left
-    if (startindex[1] > 0 && isPath(startindex, [startindex[0], startindex[1] - 1], lab)) {
-        if (((startindex[0]) == goalindex[0]) && (startindex[1] - 1 == goalindex[1])) {
-            return true;
-        } else if (cardNotinPath([startindex[0], startindex[1] - 1], path)) {
-            if (findPath([startindex[0], startindex[1] - 1], goalindex, lab, [...path, [startindex[0], startindex[1] - 1]])) { return true }
-        }
-    }
-    return false
 
-};
+function isPath(cindex1,cindex2,lab) {
+        var isPath = true;
+        var direction;
+        //direction = cindex1[0] > cindex2[0] : 
+        if (cindex1[0] > cindex2[0]) {
+            return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.top) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.bottom)
+        } else if (cindex1[0] < cindex2[0]) {
+            return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.bottom) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.top)
+        } else if (cindex1[1] > cindex2[1]) {
+            return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.left) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.right)
+        } else if (cindex1[1] < cindex2[1]) {
+            return isHalfPath(lab[cindex1[0]][cindex1[1]], DIRECTION.right) && isHalfPath(lab[cindex2[0]][cindex2[1]], DIRECTION.left)
+        } else {
+            //throw exception
+        }
+        
+
+    }
+
+    function cardNotinPath(index,path) {
+        var inPath = true
+        path.forEach((card) => {
+            if ((card[0] == index[0]) && (card[1] == index[1])) {
+                inPath = false;
+            }
+        })
+        return inPath;
+    }
+
+    /***
+    card11,card12,card13 / card21,card22, card23 / card31, card32, card33
+    player is at card31 and wants to move to card13 
+    test top > right > bottom > left
+    ***/
+    
+    export var potentialPath = [];
+    export function findPath(startindex,goalindex,lab, path) {
+
+        console.log(path);
+    
+        //test up
+        if (startindex[0] > 0 && isPath(startindex,[startindex[0] -1,startindex[1]],lab)) { 
+            if (((startindex[0] - 1) == goalindex[0]) && (startindex[1] == goalindex[1])) {
+                return true;
+            } else if (cardNotinPath([startindex[0] -1,startindex[1]],path)) {
+                if (findPath([startindex[0] -1,startindex[1]],goalindex,lab,[...path,[startindex[0] -1,startindex[1]]])) {return true}
+            }
+        }   
+        // test right
+        if (startindex[1] < lab[0].length -1 && isPath(startindex,[startindex[0],startindex[1]+1],lab)) {
+             if (((startindex[0]) == goalindex[0]) && (startindex[1] + 1 == goalindex[1])) {
+                return true;
+            } else if (cardNotinPath([startindex[0],startindex[1] + 1],path)) {
+                if (findPath([startindex[0],startindex[1] + 1],goalindex,lab,[...path,[startindex[0],startindex[1] + 1]])) { return true }
+            }
+        }
+        // test down
+        if (startindex[0] < lab.length - 1 && isPath(startindex,[startindex[0] + 1,startindex[1]],lab)) {
+             if (((startindex[0] + 1) == goalindex[0]) && (startindex[1] == goalindex[1])) {
+                return true;
+            } else if (cardNotinPath([startindex[0] + 1,startindex[1]],path)) {
+                if (findPath([startindex[0] + 1,startindex[1]],goalindex,lab,[...path,[startindex[0] + 1,startindex[1]]])) {return true}
+            }
+        }
+        // test left
+        if (startindex[1] > 0 && isPath(startindex,[startindex[0],startindex[1] - 1],lab)) {
+             if (((startindex[0]) == goalindex[0]) && (startindex[1] - 1 == goalindex[1])) {
+                return true;
+            } else if (cardNotinPath([startindex[0],startindex[1] - 1],path)) {
+                if (findPath([startindex[0],startindex[1] - 1],goalindex,lab,[...path,[startindex[0],startindex[1] - 1]])) {return true}
+            }
+        } 
+            return false
+        
+    };
